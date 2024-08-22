4da424799577508587e512f23e4e74ece0d21373
Irakli00 <141330036+Irakli00@users.noreply.github.com>
2024-08-22 16:03:22 +0400

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,10 @@
+const array = [10, 20, 30, 40, 50];
+const valueToFind = 30;
+
+if (array.some(el=>
+  el === valueToFind
+)){
+  console.log("რიცხვი ნაპოვნია")
+}else{
+  console.log("რიცხვი არ მოიძებნა")
+}
